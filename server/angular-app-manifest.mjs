5cc8bebe68cdc04ed70aecb492e59eb2f32beddc
Christina Roseline <crose00@bhn.com>
2025-04-21 23:36:44 +0530

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/UltimateTicTacToe',
+  baseHref: '/UltimateTicTacToe/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 510, hash: '1258d6ed74182bfe6b7161f4b5f0017c05457e4fa3aed24d6a58d851df7a1ff9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1023, hash: 'be2258d50f91dbd9335d1a1ed7a960936e6b2ac8123b39f82b9c1bbaa8bdec09', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 511, hash: '2212cc55ceb37f8d8a58ba1e0b92b579d3a953a9764aaae4ecc43657ee255d85', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1024, hash: '8593425cb60c7958be60a872daae66f0d33c3a391f144a59d790cf7fc0e84fec', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
